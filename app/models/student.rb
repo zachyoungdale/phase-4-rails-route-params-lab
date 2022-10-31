89538ff317d423399860bbb463028fbbc8fa5876
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-31 12:25:16 -0400

    commit tree 2145c33f626bf8aaf8f6dbc1932d861624cd45cc

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,8 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+    def self.by_name(name)
+    Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%")
+  end
+
 end
